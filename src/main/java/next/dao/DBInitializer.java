e80b38bd8b5da7246b32fea699fc1f38ada95229
ho <hataeho1@naver.com>
2015-05-27 18:23:03 +0900

    1번 완료

--- src/main/java/next/dao/DBInitializer.java
+++ src/main/java/next/dao/DBInitializer.java
@@ -1,17 +1,22 @@
 package next.dao;
 
+import javax.annotation.PostConstruct;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+import org.springframework.stereotype.Component;
 
 import core.jdbc.AbstractJdbcDaoSupport;
 
-public class DBInitializer extends AbstractJdbcDaoSupport {
+@Component
+public class DBInitializer extends AbstractJdbcDaoSupport{
 	private static final Logger logger = LoggerFactory.getLogger(DBInitializer.class);
-	
-	public DBInitializer () {
+
+	@PostConstruct
+	public void DBInit() {
 		ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
 		populator.addScript(new ClassPathResource("jwp.sql"));
 		DatabasePopulatorUtils.execute(populator, getDataSource());
